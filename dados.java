2464007290615098b669f63a1834eb26c6a28fd1
GabMoretti <114514587+GabMoretti@users.noreply.github.com>
2023-03-06 20:50:02 -0300

    Add files via upload

---
+++ dados.java
@@ -0,0 +1,12 @@
+class dados
+{
+	public static void main(String[] args){
+		System.out.println("Gabriel Moretti");
+		System.out.println("Rua Marques de abrantes,62");
+		System.out.println("Grêmio");
+		System.out.println("pizza");
+		System.out.println("09862753956");
+ 		System.out.println("Vissini Fios e Cabos");
+
+	}
+}
